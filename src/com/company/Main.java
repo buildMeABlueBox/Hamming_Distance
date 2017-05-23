514b976bd48100d9fa3edaafd7731a487142b892
Abhijit <abhivkamat@gmail.com>
2017-05-23 10:19:04 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+/**
+ * Created by Abhijit on 5/23/2017.
+ */
+public class Main {
+    /*
+    The Hamming distance between two integers is the number of
+    positions at which the corresponding bits are different.
+    Given two integers x and y, calculate the Hamming distance.
+
+                        Explanation:
+                        1   (0 0 0 1)
+                        4   (0 1 0 0)
+                               ↑   ↑
+
+       Hamming Distance for example above = 2
+
+    */
+
+    /*Runtime: O(n) - linear time. (Can also argue that since it
+      is finding hamming distance of integers that it is bound by
+      32 bits.. which would mean O(1) runtime)
+     */
+    public int hammingDistance(int x, int y) {
+        int xor = x^y;
+        int count = 0;
+
+        while (xor != 0){
+            if( (xor & 1) == 1){
+                count++;
+            }
+            xor = xor >> 1;
+        }
+        return count;
+    }
+}
